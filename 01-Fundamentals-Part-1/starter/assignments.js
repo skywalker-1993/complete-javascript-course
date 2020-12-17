26fcd83eba583cc281f660072f51b56761f0e5d3
skywalker-1993 <tiagorib93@gmail.com>
2020-12-17 19:06:16 +0000

    Updated section 1 with assignments script

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = 'Portugal';
+let continent = 'Europe';
+let population = 10.28;
+
+console.log('Country: ' + country);
+console.log('Continent: ' + continent);
+console.log('Population (in Millions): ' + population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let js = 'amazing';
+/*if (js === 'amazing') alert ('JavaScript is FUN!');
+//40 + 8 + 23 - 10;*/
+//console.log(40 + 8 + 23 - 10);
+
+//console.log('Tiago');
+//console.log(27);
+
+let firstName = 'Tiago';
+console.log(firstName);
+
+let $function = 27;
+console.log($function)
+
+let myFavoriteMovie = 'Star Wars'
+alert('Star Wars')
