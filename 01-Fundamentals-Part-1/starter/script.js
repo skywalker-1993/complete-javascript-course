e4ba76ce4a03d9c13040b72ad30055b89b3d3a3c
skywalker-1993 <tiagorib93@gmail.com>
2021-01-05 14:16:23 +0000

    updated starter scripts 05/01/21

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,58 @@
-let js = 'amazing';
-/*if (js === 'amazing') alert ('JavaScript is FUN!');
-//40 + 8 + 23 - 10;*/
+/*
+//let js = 'amazing';
+//if (js === 'amazing') alert ('JavaScript is FUN!');
+//40 + 8 + 23 - 10;
 //console.log(40 + 8 + 23 - 10);
 
 //console.log('Tiago');
 //console.log(27);
 
-let firstName = 'Tiago';
-console.log(firstName);
+// let firstName = 'Tiago';
+// console.log(firstName);
 
-let $function = 27;
-console.log($function)
+// let $function = 27;
+// console.log($function)
 
-let myFavoriteMovie = 'Star Wars'
-alert('Star Wars')
+// let myFavoriteMovie = 'Star Wars'
+// alert('Star Wars')
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1993;
+console.log(year);
+console.log(typeof year);
+
+year = null;
+console.log(year);
+console.log(typeof year);
+*/
+
+let age = 30;
+age = 31;
+
+// console.log(age);
+
+const birthYear = 1991; // Immutable variable
+// birthYear = 1990;
+
+// const job; // Must be initialized
+
+/* var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Ribeiro'; // ALWAYS DECLARE VARIABLES!!
+console.log(lastName);*/
+
+const now = 2037;
+const ageTiago = now - 1993;
+const ageAna = now - 1991;
+console.log(ageTiago, ageAna);
